9c594e3b271178558c7ccab0aa06e1af4819365d
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-17 00:16:58 +0530

    Updates

---
+++ precache-manifest.011457a01061d47dd12ec13d10d17de7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97aef6cff523b67c25cab4b4a378ade",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "60402633d90e234e61e3",
+    "url": "/ipod/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6362568a80b6229968bd",
+    "url": "/ipod/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "60402633d90e234e61e3",
+    "url": "/ipod/static/js/2.3f06a5df.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/ipod/static/js/2.3f06a5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6362568a80b6229968bd",
+    "url": "/ipod/static/js/main.312e8549.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  },
+  {
+    "revision": "a2f89af4153c498bad28733a04f49bff",
+    "url": "/ipod/static/media/Abhay.a2f89af4.jpeg"
+  },
+  {
+    "revision": "a2516e70a0588030152ecfb44834844f",
+    "url": "/ipod/static/media/home.a2516e70.jpg"
+  },
+  {
+    "revision": "3667cb397f85742100012ac3bc3bbc06",
+    "url": "/ipod/static/media/i-Like.3667cb39.mp3"
+  },
+  {
+    "revision": "8093f445d157d0f2351fd96715bb84c5",
+    "url": "/ipod/static/media/song.8093f445.jpg"
+  }
+]);
